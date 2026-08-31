90a3944fb1497832257ea7354c3521e79b5720bd
Hassan-src <hassane2881@yahoo.com>
2026-08-31 21:52:24 +0330

    DataCreation

---
+++ src/data/products.js
@@ -0,0 +1,272 @@
+export const products = [
+  {
+    id: 1,
+    name: "Grand Chesterfield",
+    category: "Sofa",
+    price: 889,
+    description:
+      "With its signature deep-buttoned tufting, elegant rolled arms and wooden legs, the Chesterfield settee's classic design dates back to the 18th century. Thanks to its distinctive features, it's no surprise that the Chesterfield sofa is the top pick for traditional-style living rooms.",
+    image: "../assets/Grand Chesterfield.avif",
+  },
+  {
+    id: 2,
+    name: "Compact 2-seater",
+    category: "Sofa",
+    price: 520,
+    description:
+      "Compact and comfy, 2-seater sofas are the basic (and failsafe) option for small living rooms.“Go for a timeless design in a neutral colour such as beige or light grey",
+    image: "../assets/Compact 2-seater.avif",
+  },
+  {
+    id: 3,
+    name: "Versatile sectional",
+    category: "Sofa",
+    price: 699,
+    description:
+      "Consisting of two or more sections joined together to create an L or U-shape, sectional sofas stay true to their name with their versatile designs. The different types of couches range from corner sofas, L-shape or chaise lounge options and even recliner sectionals.",
+    image: "../assets/Versatile sectional.avif",
+  },
+  {
+    id: 4,
+    name: "Spacious corner",
+    category: "Sofa",
+    price: 1142,
+    description:
+      "As part of the sectional sofa family tree, a corner sofa is another comfy pick for the living room especially if you regularly have guests over or have a large family.The corner sofa’s angled design makes it great for socialising. ",
+    image: "../assets/Spacious corner.avif",
+  },
+  {
+    id: 5,
+    name: "Stylish chaise lounge",
+    category: "Sofa",
+    price: 959,
+    description:
+      "Meaning ‘long chair’ in French, a chaise lounge sofa doesn’t just have a stylish name - it’s got a chic design too. With roots that date back to ancient Egyptian times, the chaise lounge started out as a combination of a chair and a daybed.",
+    image: "../assets/Stylish chaise lounge.avif",
+  },
+  {
+    id: 6,
+    name: "Relaxing recliner",
+    category: "Sofa",
+    price: 355,
+    description:
+      "If you enjoy putting your feet up after a long day - this is the sofa for you. Recliner sofas are a type of sofa and couch that reclines with the help of a footrest where the back is lowered while the front is raised. It comes in a variety of sizes such as 2-seater, 3-seater, sectional sofa and armchair.",
+    image: "../assets/Relaxing recliner.avif",
+  },
+  {
+    id: 7,
+    name: "Contemporary square arm",
+    category: "Sofa",
+    price: 1099,
+    description:
+      "Defined by its clean, sharp lines, the square arm sofa brings sleek, minimalist style to the living room. “A square arm sofa is right at home in a contemporary living room with its simple and versatile design.",
+    image: "../assets/Contemporary square arm.avif",
+  },
+  {
+    id: 8,
+    name: "Timeless mid-century",
+    category: "Sofa",
+    price: 1458,
+    description:
+      "Mid-century sofas are another favourite pick for the living room thanks to their evergreen style. Like mid-century design, this sofa style stays true to its name as it favours a modern, understated design with a minimalist aesthetic.",
+    image: "../assets/Timeless mid-century.avif",
+  },
+  {
+    id: 9,
+    name: "Elegant high-back",
+    category: "Sofa",
+    price: 659,
+    description:
+      "Another timeless and elegant design, high-back sofas provide plenty of head and neck support. “High-back sofas are a good pick for living rooms with a high ceiling.",
+    image: "../assets/Elegant high-back.avif",
+  },
+  {
+    id: 10,
+    name: "Modern wedge arm",
+    category: "Sofa",
+    price: 1599,
+    description:
+      "Another type of settee, the wedge arm sofa owes its name to its angular arms.“Sofa arm designs can range from narrow to wide.",
+    image: "../assets/Modern wedge arm.avif",
+  },
+  {
+    id: 11,
+    name: "Womb Chair",
+    category: "Chair",
+    price: 299,
+    description:
+      "with its sweeping form, offers endless postures and extra room for elbows, books or for today’s consumers, tablets",
+    image: "../assets/Womb Chair.avif",
+  },
+  {
+    id: 12,
+    name: "Adirondack",
+    category: "Chair",
+    price: 420,
+    description:
+      "Typically made of wood, the chair sits low to the ground to ensure stability on mountainous terrain, while featuring a tall slanted back, sloped seat, and wide arms for relaxation.",
+    image: "../assets/Adirondack.avif",
+  },
+  {
+    id: 13,
+    name: "Barcelona",
+    category: "Chair",
+    price: 699,
+    description:
+      "It is supremely comfortable, making it equally at home in corporate waiting rooms and private homes.",
+    image: "../assets/Barcelona.avif",
+  },
+  {
+    id: 14,
+    name: "Bentwood",
+    category: "Chair",
+    price: 566,
+    description:
+      "Lightweight and durable, Thonet’s chair featured modular components that could be packed into a one-cubic-meter box, shipped, and assembled on site.",
+    image: "../assets/Bentwood.avif",
+  },
+  {
+    id: 15,
+    name: "Bergère",
+    category: "Chair",
+    price: 789,
+    description:
+      "The bergère is similar to a fauteuil, as they are both upholstered chairs with exposed wood frames, however, the latter has open sides. Both make wonderful accent chairs.",
+    image: "../assets/Bergère.avif",
+  },
+  {
+    id: 16,
+    name: "Cantilever",
+    category: "Chair",
+    price: 335,
+    description:
+      "As opposed to a typical chair with four legs, cantilever chairs feature a single or pair of legs attached to one end of the seat and form an L-shaped base. ",
+    image: "../assets/Cantilever.avif",
+  },
+  {
+    id: 17,
+    name: "Tulip Chair",
+    category: "Chair",
+    price: 899,
+    description:
+      "The Saarinen Tulip armchair is unmistakeable, with its sleek pedestal base and super-curvy shell.",
+    image: "../assets/Tulip Chair.avif",
+  },
+  {
+    id: 18,
+    name: "Wingback Chair",
+    category: "Chair",
+    price: 822,
+    description:
+      "With tall backs and side panels that extend for each arm, the wingback chair is an ideal reading chair.",
+    image: "../assets/Wingback Chair.avif",
+  },
+  {
+    id: 19,
+    name: "Milo Club Chair",
+    category: "Chair",
+    price: 226,
+    description:
+      "Often upholstered in leather, this type of chair has rounded edges and arms, typically with a low back and armrests. Club chairs became widespread in living rooms.",
+    image: "../assets/Milo Club Chair.avif",
+  },
+  {
+    id: 20,
+    name: "Eames Lounge",
+    category: "Chair",
+    price: 347,
+    description:
+      "With a wood-and-leather seat on a six-legged base, the design represented a luxurious, modern take on the 19th-century club chair.",
+    image: "../assets/Eames Lounge.avif",
+  },
+  {
+    id: 21,
+    name: "Arco Coffee Table",
+    category: "Table",
+    price: 347,
+    description:
+      "A coffee table is not only useful for providing a surface to hold your drinks and remotes, but can also be your living room's main art display by styling it with decorative objects, candles, and succulents.",
+    image: "../assets/Arco Coffee Table.avif",
+  },
+  {
+    id: 22,
+    name: "End & Side",
+    category: "Table",
+    price: 209,
+    description:
+      "End tables and side tables are small accent tables, usually placed beside a sofa or a chair. They provide a convenient surface for your remotes and phones, while acting as decorative accents",
+    image: "../assets/End & Side.avif",
+  },
+  {
+    id: 23,
+    name: "Office Desks",
+    category: "Table",
+    price: 599,
+    description:
+      "To help keep your workplace organized, most desks feature drawers and shelves. Computer desks come with features that support modern computing setups, such as cable management, built-in monitor stands, and keyboard trays.",
+    image: "../assets/Office Desks.avif",
+  },
+  {
+    id: 24,
+    name: "Forma Console Table",
+    category: "Table",
+    price: 332,
+    description:
+      "Console tables are the most versatile type of table, both in terms of functionality and room placement. Most commonly, they're placed against a wall in hallways to display ornaments, keep keys, and hold table lamps.",
+    image: "../assets/Forma Console Table.avif",
+  },
+  {
+    id: 25,
+    name: "Bedside & Nightstands",
+    category: "Table",
+    price: 299,
+    description:
+      "Bedside tables and nightstands are the main table type in bedrooms. Their main difference is that most nightstands include drawers, while bedside tables do not. Depending on your bedroom size and tabletop space requirements, you may want a small or large nightstand.",
+    image: "../assets/Bedside & Nightstands.avif",
+  },
+  {
+    id: 26,
+    name: "Ottomans",
+    category: "Table",
+    price: 469,
+    description:
+      "Ottomans are upholstered, versatile pieces of furniture that can serve as tables or stools. Cocktail ottomans, a type of large ottomans, make great coffee tables, while small ottomans can work as end tables.",
+    image: "../assets/Ottomans.avif",
+  },
+  {
+    id: 27,
+    name: "Nesting",
+    category: "Table",
+    price: 179,
+    description:
+      "Nesting tables are sets of 2 or 3 tables with progressively lower heights and narrower widths to allow them to stack on top of each other when not in use. Individually, they can provide a personal table to each guest seated on the couch.",
+    image: "../assets/Nesting.avif",
+  },
+  {
+    id: 28,
+    name: "Aera Pedestal Table",
+    category: "Table",
+    price: 305,
+    description:
+      "Pedestal tables are defined by their single, central pedestal base instead of the conventional four legs. Pedestal dining tables are the most common type, since the main benefit of a pedestal base is that each guest has full mobility around the edge of the table thanks to the lack of any legs blocking their way.",
+    image: "../assets/Aera Pedestal Table.avif",
+  },
+  {
+    id: 29,
+    name: "C-shaped",
+    category: "Table",
+    price: 399,
+    description:
+      "As the name suggests, C-shaped tables are angled, three-sided tables that takes the form of the letter C.This iconic shape lets the table tuck snugly into the side of a couch or sectional. They work as compact end table when placed parallel to the sofa or a serving tray when pushed over the armrest.",
+    image: "../assets/C-shaped.avif",
+  },
+  {
+    id: 30,
+    name: "Nora Drum Table",
+    category: "Table",
+    price: 289,
+    description:
+      "Drum tables are single body, drum-shaped tables that provide visual weight and sculptural appeal to your interior design. They're commonly used in living rooms and lounge areas, but drum end tables are also popular.",
+    image: "../assets/Nora Drum Table.avif",
+  },
+];
